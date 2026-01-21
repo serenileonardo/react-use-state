ade6c41364fc116bd7b01c160ef33f0cb6d0269d
serenileonardo <serenileonardo.web@gmail.com>
2026-01-21 17:05:16 +0100

    feat: push finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
 import { useState } from "react"
 import languages from "./data/languages"
 import Button from "./components/Button"
+import './index.css';
+
 
 function App() {
-
-    const user = "Leonardo";
-
-    const prettyLog = (text) => {
-        console.log(`${user} ha cliccato il pulsante ${text}`);
-    }
+    const [selectedLanguage, setSelectedLanguage] = useState(languages[0])
 
     return (
         <div>
             <h1>Learn Web Development</h1>
-            {
-                languages.map(lang => {
-                    return <Button text={lang.title} onSelect={() => prettyLog(lang.title)} />
-                })
-            }
-        </div>
 
+            {/* Bottoni */}
+            <div className="button-container">
+                {languages.map((lang) => (
+                    <button className="button" key={lang.id} onClick={() => setSelectedLanguage(lang)}>
+                        {lang.title}
+                    </button>
+                ))}
+            </div>
+
+
+            <div className="card-container">
+                <div className="card">
+                    <h2>{selectedLanguage.title}</h2>
+                    <p>{selectedLanguage.description}</p>
+                </div>
+            </div>
+        </div>
     )
 }
+
+
+
 
 export default App
