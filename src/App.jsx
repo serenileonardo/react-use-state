df44c05ef227e67d8b5e3806040e90bc2697edac
serenileonardo <serenileonardo.web@gmail.com>
2026-01-21 15:21:58 +0100

    feat: add data languages and imported on app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+import languages from "./data/languages"
+
+
+function App() {
+    return (
+        <div>
+            <h1>Learn Web Development</h1>
+        </div>
+    )
+}
+
+export default App
